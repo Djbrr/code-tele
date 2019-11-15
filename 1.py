cb1ceda0a9bd348572bd5eb4cb9159b5fe2cf66b
Djbrr <56967735+Djbrr@users.noreply.github.com>
2019-11-14 22:11:38 -0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,16 @@
+firstname = input("What is your first name?")
+
+lastname = input("What is your last name?")
+location= input("Where are you?")
+age=input("How old are you?")
+space=" " 
+print("Hello"+space+firstname+space+lastname+space+"You are in"+space+location+space+"and you have"+space+age )
+
+
+
+
+
+
+
+
+
